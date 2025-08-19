0a1978ba09e2cbdc6040f2dd91788ed79ac50ba7
Aboud Zakia <abdalmalk2008123@gmail.com>
2025-08-19 19:18:21 +0300

    تحديث الملفات وإضافة صفحات Auth و Register

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth"; // صفحة تسجيل الدخول/التسجيل
-import Register from "./pages/Register"; // صفحة تسجيل ثانيه
+import Register from "./pages/register"; // صفحة تسجيل ثانيه
 
 const queryClient = new QueryClient();
 
