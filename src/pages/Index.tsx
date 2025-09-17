a41bf2c51db93c2d30e5eea59fc4213d1330fd3e
abdalmalkall <abdalmalk2008123@gmail.com>
2025-09-17 15:11:59 +0300

    وصف الcommit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,24 +4,17 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import LoginSection from "@/components/LoginSection";
 import Footer from "@/components/Footer";
-
-
-
 const Index = () => {
   const navigate = useNavigate();
-
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "instant" as ScrollBehavior });
   }, []);
-
   return (
     <div className="min-h-screen bg-background flex flex-col">
       {/* الهيدر */}
       <Header />
-
       {/* قسم البطل */}
       <HeroSection />
-
       {/* قسم تسجيل الدخول */}
       <LoginSection />
 <div className="text-center my-12 flex flex-col md:flex-row justify-center gap-6">
@@ -38,16 +31,11 @@ const Index = () => {
     onClick={() => window.location.href = "https://ecl-btec.netlify.app/"}
     className="px-8 py-3 bg-gradient-to-r from-[#b3a97c] to-[#a89c70] text-white font-medium text-lg rounded-2xl shadow-lg transform hover:scale-105 hover:shadow-xl transition-all duration-300"
   >
-    احسب معدلك
+ احسب معدلك
   </button>
 </div>
-      {/* فيديو مباشر */}
-      <div className="my-16 flex justify-center px-4">
-          متصفحك لا يدعم تشغيل الفيديو.
-      </div>
       <Footer />
     </div>
   );
 };
-
 export default Index;
